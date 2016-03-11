357184466083834824c9e355c48d28674214661d
henrique elias <hcbe2004@gmail.com>
2016-03-11 13:14:23 -0300

    fixes on test withgetting friend list

--- app/models/orkut_client.rb
+++ app/models/orkut_client.rb
@@ -5,30 +5,30 @@ class OrkutClient
 	SIGN_IN_CLIENT_URL = "login"
 
 	GET_LOGGED_USER_URL = "users/me"
-   
-    LIST_MY_FRIENDSHIPS = "friendships/me"
-	
+
+	LIST_MY_FRIENDSHIPS = "friendships/me"
+
 	def sign_in(user,password)
- 	  response = RestClient.post BASE_URL + SIGN_IN_CLIENT_URL, {username: user, password: password }
- 	  Authorizable.set_token(response.body)
- 	  response
+	  response = RestClient.post BASE_URL + SIGN_IN_CLIENT_URL, {username: user, password: password }
+	  Authorizable.set_token(response.body)
+	  response
 	end
 
 	def sign_out
 	   Authorizable.clear_token
 	end
 
-    def get_current_user_info
-    	raise "user not signed in" unless Authorizable.signed_in?
+	def get_current_user_info
+	raise "user not signed in" unless Authorizable.signed_in?
 
-    	response = RestClient::Request.execute(method: :get, url: BASE_URL + GET_LOGGED_USER_URL, headers: { :Authorization => Authorizable.get_token })
-        JSON.parse(response.body)
-    end
+	response = RestClient::Request.execute(method: :get, url: BASE_URL + GET_LOGGED_USER_URL, headers: { :Authorization => Authorizable.get_token })
+	  JSON.parse(response.body)
+	end
 
 
-    def get_my_friends
-    	raise "user not signed in" unless Authorizable.signed_in?
-    	response = RestClient::Request.execute(method: :get, url: BASE_URL + LIST_MY_FRIENDSHIPS, headers: { :Authorization => Authorizable.get_token })
-        JSON.parse(response.body)
-    end
+	def get_my_friends
+	raise "user not signed in" unless Authorizable.signed_in?
+	response = RestClient::Request.execute(method: :get, url: BASE_URL + LIST_MY_FRIENDSHIPS, headers: { :Authorization => Authorizable.get_token })
+	  JSON.parse(response.body)
+	end
 end
--- spec/models/orkut_client_spec.rb
+++ spec/models/orkut_client_spec.rb
@@ -9,6 +9,18 @@ describe OrkutClient do
     sign_in_response
   }
 
+
+  let(:friends_json){
+    %Q([{"__v":0,"vipUserRequester":false,"vipUserRequested":false,"blockUserRequester":false,"blockUserRequested":false,"status":1},{"__v":0,"vipUserRequester":false,"vipUserRequested":false,"blockUserRequester":false,"blockUserRequested":false,"status":2},{"__v":0,"vipUserRequester":false,"vipUserRequested":false,"blockUserRequester":false,"blockUserRequested":false,"status":0}])
+  }
+
+  let(:friends_stubbed_response){
+    friends_stubbed_response = double()
+    allow(friends_stubbed_response).to receive(:body).and_return(friends_json)
+    friends_stubbed_response
+  }
+
+
   it "should signin to orkut server" do
     #setup
     expect(RestClient).to receive(:post)
@@ -39,7 +51,34 @@ describe OrkutClient do
 
   it "should not perform login with invalid credentials" do
     #exercise and verify
-    expect { client.sign_in("invalid@invalid.com","invalid") }.to raise_error(Exception)
+    expect { orkut_client.sign_in("invalid@invalid.com","invalid") }.to raise_error(Exception)
   end
+
+  it "should not get the friends info with invalid credentials" do
+    #setup
+    Authorizable.clear_token
+    #exercice and verify
+    expect { orkut_client.get_my_friends }.to raise_error(Exception)
+  end
+
+
+  it "should get the friends info with valid credentials" do
+    #setup
+    expect(RestClient).to receive(:post)
+        .with(/login/,
+          hash_including(
+            username: "pedrohrs08@gmail.com",
+            password: "081289"
+          )
+        ).and_return(sign_in_response)
+    orkut_client.sign_in("pedrohrs08@gmail.com","081289")
+    allow(RestClient::Request).to receive(:execute).with(hash_including(url:/friendships\/me/)).and_return(friends_stubbed_response)
+    #exercice
+    friend_list = orkut_client.get_my_friends
+    #verify
+    expect(friend_list.length).to eq 3
+  end
+
+
 
 end
